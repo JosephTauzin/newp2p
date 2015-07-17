3828f86af204c2876422f5ea2342dc37ada91114
Joseph Tauzin <joseph.tauzin@gmail.com>
2015-07-17 18:46:26 +0000

    Day 3-verson 2

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :tests
   root 'home#index'
-
+  get  'home/about'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
